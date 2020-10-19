c2526f5df11a8b9e52a030e340f7bad03878b50c
Kyle Harris <jkcomputers_554@hotmail.com>
2020-10-19 21:27:38 +0100

    First Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ForwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Send Character forward
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime,ForceMode.VelocityChange);
+
+        //Character Left
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0 , ForceMode.VelocityChange);
+        }
+       //Character Left
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0 , ForceMode.VelocityChange);
+        }
+    }
+}
